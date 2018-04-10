ea87a079f3a8f788ccc3a25db3939ef1108a314b
Eduardo Zamarron <eduardo.zamarron.m@gmail.com>
2018-04-10 14:52:56 -0500

    sgna2

--- signal/mainp.c
+++ signal/mainp.c
@@ -2,41 +2,112 @@
 #include<stdlib.h>
 #include"imagen.h"
 
-void rgb_to_gray(unsigned char *img, uint32_t width, uint32_t height);
+unsigned char* rgb_to_gray(unsigned char *img, uint32_t width, uint32_t height);
+void gray_to_rgb(unsigned char *imagenGray, unsigned char *imagenRGB, uint32_t width, uint32_t height)
 
 int main(void)
 {
 	bmpInfoHeader bmpinfo;
-	unsigned char *imagen;
+	unsigned char *imagenRGB, *imagenGray;
 	
-	imagen = abrirBMP("hoja.bmp", &bmpinfo);
+	imagenRGB = abrirBMP("hoja.bmp", &bmpinfo);
 	displayInfo(&bmpinfo);
 
-	rgb_to_gray(imagen, bmpinfo.width, bmpinfo.height );
+	imagenGray = rgb_to_gray(imagenRGB, bmpinfo.width, bmpinfo.height );
 	
-	guardarBMP("hoja_gris_1.bmp",&bmpinfo,imagen);
+	brilloImagen(imagenGray, bmpinfo.width, bmpinfo.height);
 	
-	free(imagen);
+	
+	gray_to_rgb(imagenGray,imagenRGB,bmpinfo.width, bmpinfo.height);
+	
+	guardarBMP("hoja_gris_1.bmp",&bmpinfo,imagenRGB);
+	
+	
+	
+	free(imagenRGB);
+	free(imagenGray);
 	
 	return 0;
 }
 
-void rgb_to_gray(unsigned char *imagen, uint32_t width, uint32_t height)
+void brilloImagen(unsigned char *imagenGray, uint32_t width, uint32_t height)
+{
+			register int x,y;
+	int index,indexGray;
+	int nivel_gris;
+	int brillo;
+	
+	for( y = 0 ; y < height ; y++)
+	{
+		for( x = 0 ; x < width ; x++)
+		{
+			indexGray = (y * width + x) ;
+			index = indexGray * 3;
+			
+			brillo = imagenGray[indexGray] + 40;
+			imagenGray[indexGray] = (brillo > 255)? 255: brillo;
+		}
+	}
+}	
+
+void gray_to_rgb(unsigned char *imagenGray, unsigned char *imagenRGB, uint32_t width, uint32_t height)
 {
 	register int x,y;
-	int index;
+	int index,indexGray;
 	int nivel_gris;
 	
 	for( y = 0 ; y < height ; y++)
 	{
 		for( x = 0 ; x < width ; x++)
 		{
-			index = (y * width + x) * 3;
-			nivel_gris = imagen[index] + imagen[index+1] + imagen[index+2];
-			nivel_gris /= 3;
-			imagen[index] = nivel_gris;
-			imagen[index+1] = nivel_gris;
-			imagen[index+2] = nivel_gris;
+			indexGray = (y * width + x) ;
+			index = indexGray * 3;
+			 
+			nivel_gris = imagenGray[indexGray];
+			
+			imagenRGB[index]   = nivel_gris;
+			imagenRGB[index+1] = nivel_gris;
+			imagenRGB[index+2] = nivel_gris;
+			//nivel_gris /= 3;
+			/*
+			imagenRGB[index] = nivel_gris;
+			imagenRGB[index+1] = nivel_gris;
+			imagenRGB[index+2] = nivel_gris;
+			* 
+			* */
 		}
 	}
+}
+
+unsigned char* rgb_to_gray(unsigned char *imagenRGB, uint32_t width, uint32_t height)
+{
+	register int x,y;
+	int index,indexGray;
+	int nivel_gris;
+	unsigned char *imagenGray;
+	
+	imagenGray = (unsigned char*)malloc(width*height*sizeof(unsigned char));
+	
+	for( y = 0 ; y < height ; y++)
+	{
+		for( x = 0 ; x < width ; x++)
+		{
+			indexGray = (y * width + x) ;
+			index = indexGray * 3;
+			 
+			nivel_gris =(
+				11*imagenRGB[index] + 
+				59*imagenRGB[index+1] + 
+				60*imagenRGB[index+2]) / 100 ;
+			//nivel_gris /= 3;
+			/*
+			imagenRGB[index] = nivel_gris;
+			imagenRGB[index+1] = nivel_gris;
+			imagenRGB[index+2] = nivel_gris;
+			* 
+			* */
+			imagenGray[indexGray] = nivel_gris;
+		}
+	}
+	return imagenGray;
 }
