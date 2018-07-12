3dc246cbb6785c5e0bcdc93b9ecd3a952e79b4b8
Lain Iwakura <lain@z200.nexo>
2018-07-12 15:55:15 -0500

    read serial

---
+++ read_serial.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+int config_serial( char *dispositivo_serial, speed_t baudios );
+
+int main(int argc, char* argv[])
+{
+	//register int i;
+	//register int count;
+	int fd_serie;
+	char dato;
+	//char buffer[1000];
+	int vel;
+	printf("Iniciando device %s con velocidad de %s \n",argv[1],argv[2]);
+	vel = atoi(argv[2]);
+	
+	// Abrir comunicacion serial
+	switch (vel)
+	{
+		case 1:
+			fd_serie = config_serial( argv[1], B115200 );
+			break;
+		case 2:
+			fd_serie = config_serial( argv[1], B230400 );
+			break;
+		case 3:
+			fd_serie = config_serial( argv[1], B9600 );
+			break;
+		case 4:
+			fd_serie = config_serial( argv[1], B19200 );
+			break;
+		case 5:
+			fd_serie = config_serial( argv[1], B38400 );
+			break;
+		case 6:
+			fd_serie = config_serial( argv[1], B57600 );
+			break;
+		case 7:
+			fd_serie = config_serial( argv[1], B1200 );
+			break;
+		case 8:
+			fd_serie = config_serial( argv[1], B2400 );
+			break;
+		case 9:
+			fd_serie = config_serial( argv[1], B4800 );
+			break;
+		default:
+			printf("Opcion incorrecta\n");
+			exit(EXIT_FAILURE);
+			break;
+	}
+	//printf("Serial abierto con descriptor: %d\n", fd_serie);
+	
+	// Leer continuamente los datos del GPS
+	while(1)
+	{
+		read ( fd_serie, &dato, 1 );
+		printf("%c",dato);
+	}
+}
+
+/** @brief: Esta funcion Configura la interfaz serie
+ *  @param: dispositivo_serial, Nombre el dispositivo serial a usar: ttyUSB0, ttyUSB1, etc
+ *  @param: baudios, Velocidad de comunicacion. Se usa la constante Bxxxx, donde xxxx  es la
+ *          velocidad. Estan definidas en termios.h. Ejemplo: B9600, B19200..
+ *  @return: fd, Descriptor del serial
+ *******************************************************************************************
+ */
+int config_serial( char *dispositivo_serial, speed_t baudios )
+{
+	struct termios newtermios;
+  	int fd;
+/*
+ * Se abre un descriptor de archivo para manejar la interfaz serie
+ * O_RDWR - Se abre el descriptor para lectura y escritura
+ * O_NOCTTY - El dispositivo terminal no se convertira en el terminal del proceso
+ * ~O_NONBLOCK - Se hace bloqueante la lectura de datos
+ */
+  	fd = open( dispositivo_serial, (O_RDWR | O_NOCTTY) & ~O_NONBLOCK );
+	if( fd == -1 )
+	{
+		printf("Error al abrir el dispositivo tty \n");
+		exit( EXIT_FAILURE );
+  	}
+/*
+ * cflag - Proporciona los indicadores de modo de control
+ *	CBAUD	- Velocidad de transmision en baudios.
+ * 	CS8	- Especifica los bits por dato, en este caso 8
+ * 	CLOCAL 	- Ignora las lineas de control del modem: CTS y RTS
+ * 	CREAD  	- Habilita el receptor del UART
+ * iflag - proporciona los indicadores de modo de entrada
+ * 	IGNPAR 	- Ingnora errores de paridad, es decir, comunicación sin paridad
+ * oflag - Proporciona los indicadores de modo de salida
+ * lflag - Proporciona indicadores de modo local
+ * 	TCIOFLUSH - Elimina datos recibidos pero no leidos, como los escritos pero no transmitidos
+ * 	~ICANON - Establece modo no canonico, en este modo la entrada esta disponible inmediatamente
+ * cc[]	 - Arreglo que define caracteres especiales de control
+ *	VMIN > 0, VTIME = 0 - Bloquea la lectura hasta que el numero de bytes (1) esta disponible
+ */
+	newtermios.c_cflag 	= CBAUD | CS8 | CLOCAL | CREAD;
+  	newtermios.c_iflag 	= IGNPAR;
+  	newtermios.c_oflag	= 0;
+  	newtermios.c_lflag 	= TCIOFLUSH | ~ICANON;
+  	newtermios.c_cc[VMIN]	= 1;
+  	newtermios.c_cc[VTIME]	= 0;
+
+// Configura la velocidad de salida del UART
+  	if( cfsetospeed( &newtermios, baudios ) == -1 )
+	{
+		printf("Error al establecer velocidad de salida \n");
+		exit( EXIT_FAILURE );
+  	}
+// Configura la velocidad de entrada del UART
+	if( cfsetispeed( &newtermios, baudios ) == -1 )
+	{
+		printf("Error al establecer velocidad de entrada \n" );
+		exit( EXIT_FAILURE );
+	}
+// Limpia el buffer de entrada
+	if( tcflush( fd, TCIFLUSH ) == -1 )
+	{
+		printf("Error al limpiar el buffer de entrada \n" );
+		exit( EXIT_FAILURE );
+	}
+// Limpia el buffer de salida
+	if( tcflush( fd, TCOFLUSH ) == -1 )
+	{
+		printf("Error al limpiar el buffer de salida \n" );
+		exit( EXIT_FAILURE );
+	}
+/*
+ * Se establece los parametros de terminal asociados con el
+ * descriptor de archivo fd utilizando la estructura termios
+ * TCSANOW - Cambia los valores inmediatamente
+ */
+	if( tcsetattr( fd, TCSANOW, &newtermios ) == -1 )
+	{
+		printf("Error al establecer los parametros de la terminal \n" );
+		exit( EXIT_FAILURE );
+	}
+//Retorna el descriptor de archivo
+	return fd;
+}
